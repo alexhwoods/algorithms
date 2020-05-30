ae3f10042866a4e925b279f0152d24979a70bbdf
Alexander Woods <alex.w@ware2go.co>
2020-05-29 21:30:45 -0400

    Remove unused imports

--- min-swaps/kotlin/minSwaps.kt
+++ min-swaps/kotlin/minSwaps.kt
@@ -1,23 +1,4 @@
-package io.atomiccommits.min-swaps
-
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
-import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
 import kotlin.collections.*
-import kotlin.comparisons.*
-import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
 
 
 fun minimumSwaps(arr: Array<Int>): Int {
